172d6a21e1bc4bcad7f743374fecb57a54635f3a
Lars Timmerman <infigoforza@gmail.com>
2024-12-05 20:53:12 +0100

    Day 5 part 1

--- lib/advent_of_code/day_05.ex
+++ lib/advent_of_code/day_05.ex
@@ -1,7 +1,67 @@
 defmodule AdventOfCode.Day05 do
-  def part1(_args) do
+  def part1(input) do
+    [instructions, pages] = parse_input(input)
+
+    Enum.map(pages, fn page ->
+      is_valid? =
+        instructions
+        |> Enum.map(&check_instructions(&1, page))
+        |> Enum.all?()
+
+      if is_valid? do
+        index = ((length(page) - 1) / 2) |> round()
+        page |> Enum.at(index) |> String.to_integer()
+      else
+        0
+      end
+    end)
+    |> Enum.sum()
   end
 
-  def part2(_args) do
+  def part2(input) do
+    [instructions, pages] = parse_input(input)
+
+    Enum.map(pages, fn page ->
+      is_valid? =
+        instructions
+        |> Enum.map(&check_instructions(&1, page))
+        |> Enum.all?()
+
+      if is_valid? do
+        index = ((length(page) - 1) / 2) |> round()
+        page |> Enum.at(index) |> String.to_integer()
+      else
+        0
+      end
+    end)
+    |> Enum.sum()
+  end
+
+  defp check_instructions(_, _page, keep_checking \\ true)
+  defp check_instructions(_, _page, false), do: false
+  defp check_instructions([], _page, is_valid), do: is_valid
+
+  defp check_instructions([left, right | instructions], page, is_valid) do
+    if left in page and right in page do
+      li = Enum.find_index(page, &(&1 == left))
+      ri = Enum.find_index(page, &(&1 == right))
+
+      check_instructions(instructions, page, li < ri)
+    else
+      check_instructions(instructions, page, is_valid)
+    end
+  end
+
+  defp parse_input(input) do
+    [instructions, rows] =
+      input
+      |> String.split(~r/^\n/m, trim: true)
+
+    rules =
+      instructions |> String.split("\n", trim: true) |> Enum.map(&String.split(&1, "|"))
+
+    pages = rows |> String.split("\n", trim: true) |> Enum.map(&String.split(&1, ","))
+
+    [rules, pages]
   end
 end
